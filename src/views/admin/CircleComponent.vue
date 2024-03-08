8b2af57ad2dfdacc2b43cdf4bf93073c2068ed8f
Abhay Kumar <abhayku2002@gmail.com>
2024-03-08 17:32:15 +0530

    Create CircleComponent.vue

---
+++ src/views/admin/CircleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-sheet
+    class="ml-13 mb-8 circle"
+    height="20"
+    width="20"
+  ></v-sheet>
+</template>
+
+<script>
+export default {
+  props: ['size', 'color'],
+};
+</script>
