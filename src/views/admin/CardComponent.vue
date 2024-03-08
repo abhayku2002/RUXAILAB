c8fd9f72b6cd8efde595b986710033143c482435
Abhay Kumar <abhayku2002@gmail.com>
2024-03-08 17:30:56 +0530

    Create CardComponent.vue

---
+++ src/views/admin/CardComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card
+    hover
+    color="white"
+    class="cards ml-5 mr-5 group"
+    height="350"
+    @click="$emit('click')"
+  >
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['imgSrc', 'title', 'subtitle', 'features'],
+};
+</script>
