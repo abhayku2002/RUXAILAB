598648dc81ee3ea81340650beca92a9502b8edf3
Abhay Kumar <abhayku2002@gmail.com>
2024-03-08 17:31:44 +0530

    Create DialogComponent.vue

---
+++ src/views/admin/DialogComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-dialog v-model="dialog" fullscreen transition="dialog-bottom-transition">
+    <v-card color="#f9f5f0">
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ['dialog', 'title', 'subtitle', 'fields'],
+  methods: {
+    closeDialog() {
+      this.$emit('close');
+    },
+    submitForm() {
+      this.$emit('submit');
+    },
+  },
+};
+</script>
